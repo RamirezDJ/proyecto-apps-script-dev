730582d9bb09ca6f780cccbab95051b3826434dc
RamirezDJ <l21070064@valladolid.tecnm.mx>
2025-02-27 18:32:55 -0600

    Video 14

--- backend/controllers/controllerUsuario.js
+++ backend/controllers/controllerUsuario.js
@@ -13,9 +13,25 @@ function guardarUsuario(usuario){
         };
     }
         
-    }
+}
     
     function listarUsuarios(id = undefined) {
         // return obtenerDatos(env_().SH_REGISTRO_USUARIOS);
         return JSON.stringify(_read(obtenerSheet(env_().SH_REGISTRO_USUARIOS), id));
+    }
+
+    function actualizarUsuario(id, datos) {
+        try {
+            const sheetUsuarios = obtenerSheet(env_().SH_REGISTRO_USUARIOS);
+            Update(id, datos, sheetUsuarios);
+            return {
+                titulo: "Actualizado correctamente",
+                descripcion: "Ya se encuentra el usuario actualizado en la base de datos."
+            };
+        } catch (error) {
+            return{
+                titulo: "Ops ha ocurrido un error! "+ error,
+                descripcion: "Porfavor contacte a soporte."
+            };
+        }
     }
